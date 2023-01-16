314847101abce267ff4279ed6c47dbb0e498ebe3
Luis Vallejo <luisern29@gmail.com>
2023-01-16 01:33:36 -0500

    procediendo a despliegue en render

--- src/tools/database.js
+++ src/tools/database.js
@@ -3,10 +3,18 @@ const config = require('../config')
 
 const db = new Sequelize({
     dialect: 'postgres',
-    host: config.db.host, //? Variable de entorno del host
-    username: config.db.username, //? Variable de entorno del usuario
-    password: config.db.password, //? Variable de entorno de la contraseña
-    database: config.db.dbName //? Variable de entorno de la base de datos
+    host: config.db.host, // - Variable de entorno del host - //
+    username: config.db.username, // - Variable de entorno del usuario - //
+    password: config.db.password, // - Variable de entorno de la contraseña - //
+    database: config.db.dbName, // - Variable de entorno de la base de datos - //
+    dialectOptions: 
+        process.env.NODE_ENV === 'production'
+            ? {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            } : {}
 })
 
 module.exports = db
